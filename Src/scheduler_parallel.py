e13770919190da8547e12d0febd0313a324887f9
11yyaa <113608806+11yyaa@users.noreply.github.com>
2026-04-20 12:48:50 +0300

    Update scheduler_parallel.py

--- Src/scheduler_parallel.py
+++ Src/scheduler_parallel.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
-from typing import Dict
+from typing import Dict, Optional
 
 from Src.base_scheduler import BaseScheduler
 from Include.Scheduler.IFaces.task_spec import TaskSpec
-from Include.Scheduler.IFaces.utils import  id_sort_key
+from Include.Scheduler.IFaces.utils import id_sort_key
 
-#параллельный планировщик, в workers нет аргументов, 
-class ParallelFIFODepScheduler(BaseScheduler):
+#Последовательный планировщик с учетом зависимостей, но workers = 1
+class SequentialDepsScheduler(BaseScheduler):
     def get_name(self) -> str:
-        return "parallel"
+		return "sequential"
+
+    def _effective_workers(self) -> int:
+        return 1
 
     def _make_ready_key(self, tasks: Dict[str, TaskSpec]):
         return lambda tid: (id_sort_key(tid),)
