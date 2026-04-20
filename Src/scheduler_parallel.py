897f2792613acdf589a3a63048b66c335a0767f6
11yyaa <113608806+11yyaa@users.noreply.github.com>
2026-04-20 12:48:29 +0300

    Update scheduler_parallel.py

--- Src/scheduler_parallel.py
+++ Src/scheduler_parallel.py
@@ -2,9 +2,9 @@
 
 from typing import Dict
 
-from base_scheduler import BaseScheduler
-from task_spec import TaskSpec
-from utils import  id_sort_key
+from Src.base_scheduler import BaseScheduler
+from Include.Scheduler.IFaces.task_spec import TaskSpec
+from Include.Scheduler.IFaces.utils import  id_sort_key
 
 #параллельный планировщик, в workers нет аргументов, 
 class ParallelFIFODepScheduler(BaseScheduler):
